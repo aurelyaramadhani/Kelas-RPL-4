e61a6353d4de7cfa9469148e2f664d7122e0756d
aurelyaramadhani <aurelya_ramadhani_34rpl@student.smktelkom-mlg.sch.id>
2025-08-20 18:59:52 +0700

    Add files via upload

---
+++ public class AccountTest {.java
@@ -0,0 +1,13 @@
+public class AccountTest {
+   public static void main(String[] args){
+
+        SavingsAccount sa0001 = new SavingsAccount();
+        sa0001.balance = 1000;
+        sa0001.name = "Damien";
+        sa0001.interestRate = 0.02;
+
+        SavingsAccount sa0002 = new SavingsAccount();
+        sa0002.balance = 2000;
+        sa0002.name = "Bill";
+    }//end method main
+}//end class AccountTest
